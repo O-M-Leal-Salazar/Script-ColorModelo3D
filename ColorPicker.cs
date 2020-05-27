56454671e1552c155d58cc6b78d4df7b476487b2
Oscar Leal Salazar <65517207+O-M-Leal-Salazar@users.noreply.github.com>
2020-05-27 17:36:27 -0500

    Add files via upload

---
+++ ColorPicker.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPicker : MonoBehaviour
+{
+
+    public Material[] BodyColorMat;
+    Material CurrMat;
+    Renderer renderer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        renderer = this.GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void GrisMaterial()
+    {
+        renderer.material = BodyColorMat[0];
+        CurrMat = renderer.material;
+    }
+
+    public void AmarilloMaterial()
+    {
+        renderer.material = BodyColorMat[1];
+        CurrMat = renderer.material;
+    }
+
+    public void RojoMaterial()
+    {
+        renderer.material = BodyColorMat[2];
+        CurrMat = renderer.material;
+    }
+
+    public void TransMaterial()
+    {
+        renderer.material = BodyColorMat[3];
+        CurrMat = renderer.material;
+    }
+}
